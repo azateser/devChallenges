9c4203c57ad95589cbb2fe6a65cbe99ade273c8b
Azat ESER <developer.azat@gmail.com>
2022-10-08 23:44:57 +0300

    old

--- 01 - Responsive Web Developer/01-404-not-found/pages/index.js
+++ 01 - Responsive Web Developer/01-404-not-found/pages/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 import Head from 'next/head'
 
-import Scarecow from '../image/Scarecrow.png'
-
 const Home = () => {
   return (
     <React.Fragment>
@@ -15,7 +13,7 @@ const Home = () => {
         <div className='font-inconsolata tracking-tighter lg:px-20 px-7 lg:py-9 py-5 text-2xl'>404 NOT FOUND</div>
 
         <div className='flex lg:flex-row flex-col items-start justify-center lg:px-20 px-8 lg:my-40 my-10'>
-          <div className='lg:w-2/3  w-full lg:pr-28'><img src={Scarecow} alt="Scarecrow" /></div>
+          <div className='lg:w-2/3  w-full lg:pr-28'><img src="./Scarecrow.png" alt="Scarecrow" /></div>
           <div className='flex flex-col md:mt-3 mt-16'>
             <div className='font-bold lg:text-6xl text-5xl leading-normal'>I have bad news for you</div>
             <div className='w-96 lg:text-2xl text-xl text-[#4F4F4F] mt-8'>The page you are looking for might be removed or is temporarily unavailable</div>
